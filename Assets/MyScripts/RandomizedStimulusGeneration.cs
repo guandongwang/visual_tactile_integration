6461fad15d21ba482d6a5a3c494f51f0f3d63734
guandongwang <guandong.wang@outlook.com>
2023-05-11 09:38:12 +1000

    Updated Randomized Stim Creation

---
+++ Assets/MyScripts/RandomizedStimulusGeneration.cs
@@ -0,0 +1,213 @@
+using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = System.Random;
+using System.Linq;
+
+public class RandomizedStimulusGeneration : MonoBehaviour
+{
+    List<List<Stimulus>> SessionStimulusCollection;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    List<List<Stimulus>> CreateSessionStimulus()
+    {
+        List<string > tactileDisks = new List<string>() { "B3", "B7", "B11", "B15", "B19" };
+        List<string> visualDisk = new List<string>() { "B3", "B7", "B11", "B15", "B19" };
+        List<string> visualDiskLow = new List<string>() { "B1", "B3", "B7", "B11", "B15"};
+        List<string> visualDiskHigh = new List<string>() { "B3", "B7", "B11", "B15", "B19", "B21" };
+        string referenceDisk = "B11";
+
+        List<Stimulus> stimulusCollection = new List<Stimulus>();
+
+        //visual ref/test
+        for (int i = 0; i <5; i++)
+        {
+            Stimulus stimulus = new Stimulus();
+            stimulus.Condition = "Vision";
+            stimulus.StimPairNumber = i + 1;
+            stimulus.S1Vision = referenceDisk;
+            stimulus.S2Vision = visualDisk[i];
+            stimulus.S1Touch = "dummy";
+            stimulus.S2Touch = "dummy";
+
+            stimulusCollection.Add(stimulus);
+        }
+        //visual test/ref
+        for (int i = 0; i < 5; i++)
+        {
+            Stimulus stimulus = new Stimulus();
+            stimulus.Condition = "Vision";
+            stimulus.StimPairNumber = i + 6;
+            stimulus.S1Vision = visualDisk[i];
+            stimulus.S2Vision = referenceDisk;
+            stimulus.S1Touch = "dummy";
+            stimulus.S2Touch = "dummy";
+            stimulusCollection.Add(stimulus);
+        }
+
+        //Touch
+        for (int i = 0; i < 5; i++)
+        {
+            Stimulus stimulus = new Stimulus();
+            stimulus.Condition = "Touch";
+            stimulus.StimPairNumber = i + 1;
+            stimulus.S1Vision = "dummy";
+            stimulus.S2Vision = "dummy";
+            stimulus.S1Touch = referenceDisk;
+            stimulus.S2Touch = tactileDisks[i];
+
+            stimulusCollection.Add(stimulus);
+        }
+
+        for (int i = 0; i < 5; i++)
+        {
+            Stimulus stimulus = new Stimulus();
+            stimulus.Condition = "Touch";
+            stimulus.StimPairNumber = i + 6;
+            stimulus.S1Vision = "dummy";
+            stimulus.S2Vision = "dummy";
+            stimulus.S1Touch = tactileDisks[i];
+            stimulus.S2Touch = referenceDisk;
+            stimulusCollection.Add(stimulus);
+        }
+
+        //Combine
+        for (int i = 0; i < 5; i++)
+        {
+            Stimulus stimulus = new Stimulus();
+            stimulus.Condition = "Touch";
+            stimulus.StimPairNumber = i + 1;
+            stimulus.S1Vision = referenceDisk;
+            stimulus.S2Vision = visualDisk[i];
+            stimulus.S1Touch = referenceDisk;
+            stimulus.S2Touch = tactileDisks[i];
+
+            stimulusCollection.Add(stimulus);
+        }
+
+        for (int i = 0; i < 5; i++)
+        {
+            Stimulus stimulus = new Stimulus();
+            stimulus.Condition = "Touch";
+            stimulus.StimPairNumber = i + 6;
+            stimulus.S1Vision = visualDisk[i];
+            stimulus.S2Vision = referenceDisk;
+            stimulus.S1Touch = tactileDisks[i];
+            stimulus.S2Touch = referenceDisk;
+            stimulusCollection.Add(stimulus);
+        }
+
+        //VLowerFreqThanT
+        for (int i = 0; i < 5; i++)
+        {
+            Stimulus stimulus = new Stimulus();
+            stimulus.Condition = "VLowerFreqThanT";
+            stimulus.StimPairNumber = i + 1;
+            stimulus.S1Vision = referenceDisk;
+            stimulus.S2Vision = visualDiskLow[i];
+            stimulus.S1Touch = referenceDisk;
+            stimulus.S2Touch = tactileDisks[i];
+
+            stimulusCollection.Add(stimulus);
+        }
+
+        for (int i = 0; i < 5; i++)
+        {
+            Stimulus stimulus = new Stimulus();
+            stimulus.Condition = "VLowerFreqThanT";
+            stimulus.StimPairNumber = i + 6;
+            stimulus.S1Vision = visualDiskLow[i];
+            stimulus.S2Vision = referenceDisk;
+            stimulus.S1Touch = tactileDisks[i];
+            stimulus.S2Touch = referenceDisk;
+            stimulusCollection.Add(stimulus);
+        }
+        //VHigherFreqThanT
+        for (int i = 0; i < 5; i++)
+        {
+            Stimulus stimulus = new Stimulus();
+            stimulus.Condition = "Touch";
+            stimulus.StimPairNumber = i + 1;
+            stimulus.S1Vision = referenceDisk;
+            stimulus.S2Vision = visualDiskHigh[i];
+            stimulus.S1Touch = referenceDisk;
+            stimulus.S2Touch = tactileDisks[i];
+
+            stimulusCollection.Add(stimulus);
+        }
+
+        
+        for (int i = 0; i < 5; i++)
+        {
+            Stimulus stimulus = new Stimulus();
+            stimulus.Condition = "Touch";
+            stimulus.StimPairNumber = i + 6;
+            stimulus.S1Vision = visualDiskHigh[i];
+            stimulus.S2Vision = referenceDisk;
+            stimulus.S1Touch = tactileDisks[i];
+            stimulus.S2Touch = referenceDisk;
+            stimulusCollection.Add(stimulus);
+        }
+
+        //repeat 50 stim 10 times, total 500 trials
+        List<Stimulus> randomizedStim = ShuffleList(RepeatList(stimulusCollection, 10));
+
+        List<List<Stimulus>> SessionStimulusCollection = new List<List<Stimulus>>();
+
+        int startIndex = 0;
+        int blockTrialNumber = 50;
+        for (int i = 0; i < 10; i++)
+        {
+
+            // Create a new smaller list and add the range of items to it
+            List<Stimulus> blockStim = randomizedStim.GetRange(startIndex, blockTrialNumber);
+            SessionStimulusCollection.Add(blockStim);
+
+            // Update the start index for the next iteration
+            startIndex += blockTrialNumber;
+        }
+        return SessionStimulusCollection;
+
+    }
+
+
+    List<T> RepeatList<T>(List<T> originalList, int repeatCount)
+    {
+        List<T> repeatedList = new List<T>();
+
+        // Repeat the original list the specified number of times
+        for (int i = 0; i < repeatCount; i++)
+        {
+            repeatedList.AddRange(originalList);
+        }
+        return repeatedList;
+    }
+
+
+
+    List<T> ShuffleList<T>(List<T> listToShuffle)
+    {
+        Random rng = new Random();
+        for (int i = listToShuffle.Count - 1; i > 0; i--)
+        {
+            var k = rng.Next(i + 1);
+            var value = listToShuffle[k];
+            listToShuffle[k] = listToShuffle[i];
+            listToShuffle[i] = value;
+        }
+        return listToShuffle;
+    }
+}
+
+
---
+++ Assets/MyScripts/Stimulus.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stimulus 
+{
+    public string Condition { get; set; }
+    public int StimPairNumber { get; set; }
+    public string S1Vision { get; set; }
+    public string S2Vision { get; set; }
+    public string S1Touch { get; set; }
+    public string S2Touch { get; set; }
+}
+
